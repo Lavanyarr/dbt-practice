5541ed9202b8e649721f4bf6051483931d361458
Lavanya <lavanya19091997@gmail.com>
2023-04-01 14:33:26 +0530

    checking git connection

--- models/stripe/stg_payments.sql
+++ models/stripe/stg_payments.sql
@@ -1,8 +1,16 @@
 
 WITH payments as (
     select
-        *
-    from {{source('stripe','payments')}}
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    amount / 100 as amount,
+    created as created_at
+
+from `dbt-tutorial`.`stripe`.`payment`
 )
 
 select * from payments 
+
+--testing commit
